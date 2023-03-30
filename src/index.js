06556bb9d5a9838c104f772128ebcdd9f66d3dac
manish <manishstha44@gmail.com>
2023-03-30 19:23:09 +0545

    GalleryImage

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import './index.css'
+const name = "Manish Shrestha";
+const img1= "https://picsum.photos/200/300";
+const img2= "https://picsum.photos/250/300";
+const img3= "https://picsum.photos/250/350";
+const links= "https://www.facebook.com";
+ReactDOM.render(
+    <>
+        <h1 className="heading">My name is {name}</h1>
+        <div className="img_div">
+        <img src= {img1} alt="random picture"/>
+        <img src= {img2} alt="random picture"/>
+        <img src= {img3} alt="random picture"/>
+        <a href= {img2} target="_blank">
+            <img src = {img1} alt="random picture" />
+        </a>
+        </div>
+    </>,
+    document.getElementById("root")
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
